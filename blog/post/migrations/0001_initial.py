5bd70f6d696d1d5157ac05ecd6525987caeb140b
bekbo1sunn <bekbolsun2003asanov@gmail.com>
2023-04-11 12:03:54 +0600

    post listing with templates

--- blog/post/migrations/0001_initial.py
+++ blog/post/migrations/0001_initial.py
@@ -24,4 +24,4 @@ class Migration(migrations.Migration):
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
             ],
         ),
-    ]
+     ]
