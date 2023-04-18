d29490cb9b9bd377d591e6554a1ecc5f844d0709
bekbo1sunn <bekbolsun2003asanov@gmail.com>
2023-04-18 11:53:00 +0600

    comments

--- blog/review/views.py
+++ blog/review/views.py
@@ -1,9 +1,13 @@
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
-from .models import Like
+
+from .models import Like, Comment
 from post.models import Post, User
+from .serializers import CommentSerializer
 
 
 @api_view(['POST'])
@@ -19,3 +23,19 @@ def toggle_like(request, id):
         # если нет, создаем
         Like.objects.create(user=user, post=post)
     return Response(status=201)
+
+
+class CreateCommentAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+class UpdateCommentAPIView(UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeleteCommentAPIView(DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
